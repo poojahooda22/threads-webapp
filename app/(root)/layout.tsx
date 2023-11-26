cb5339375a784f48899d4ea742258687658252cd
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-26 22:25:23 +0530

    updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -18,7 +18,23 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main>
+            <LeftSidebar />
+
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
