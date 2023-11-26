d388d9b8ec3ed731905fb3c804413749c1e3319f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-26 20:59:01 +0530

    updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Home page!
-    </main>
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
   )
 }
