6a04c2f6d5ac75c60e0d4faccca44019f4f4b6d2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-27 12:35:07 +0530

    create topbar

---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+function Topbar() {
+    return (
+        <nav className="topbar">
+            <Link href="/" className="flex items-center gap-4">
+                <Image
+                    src="/assets/logo.svg"
+                    alt=""
+                    width={28}
+                    height={28}
+                />
+                <p className="text-headings3-bold text-light-1 
+                max-xs:hidden">Threads</p>
+            </Link>
+             
+        </nav>
+    )
+}
+
+export default Topbar;
