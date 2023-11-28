b7d8ada064c5e407dddefb24bf42700a828c61d9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-28 14:15:47 +0530

    updated topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,5 @@
+import { SignedIn, OrganizationSwitcher, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -15,6 +17,32 @@ function Topbar() {
                 <p className="text-headings3-bold text-light-1 
                 max-xs:hidden">Threads</p>
             </Link>
+
+            <div className="flex items-center gap-1">
+                <div className="block md:hidden">
+                    <SignedIn>
+                        <SignOutButton>
+                            <div className='flex cursor-pointer'>
+                                <Image
+                                    src="/assets/logout.svg"
+                                    alt="logout"
+                                    width={24}
+                                    height={24}
+                                />
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
+
+                <OrganizationSwitcher
+                    appearance={{
+                        baseTheme: dark,
+                        elements: {
+                            organizationSwitcherTrigger: "py-2 px-4"
+                        }
+                    }}
+                />
+            </div>
              
         </nav>
     )
