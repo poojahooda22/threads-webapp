fa14865509e9641d5e6347cd88c44ff3f2620780
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-28 14:15:02 +0530

    create account profile

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    }
+    btnTitle: string;
+}
+
+const AccountProfile = ({ user, btnTitle}: Props) => {
+  return (
+    <div>
+      Account Profile
+    </div>
+  )
+}
+
+export default AccountProfile;
