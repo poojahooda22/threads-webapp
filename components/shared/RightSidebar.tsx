f72f00e40c9654c5ddc6b635eba3bd04aefad585
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-28 20:49:16 +0530

    updated

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -6,7 +6,8 @@ function RightSidebar() {
         <section 
             className="custom-scrollbar rightsidebar"
         >
-            <div className="flex flex-1 flex-col justify-start">
+            <div 
+            className="flex flex-1 flex-col justify-start">
                 <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
             </div>
 
