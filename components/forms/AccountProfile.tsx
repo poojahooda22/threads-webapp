0eaf192f97b347328cd42d9505faa5528cc88902
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-11-29 21:40:28 +0530

    updated accountprofile

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -14,6 +14,7 @@ import {
   UserValidation
 } from '@/lib/validations/user';
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 interface Props {
