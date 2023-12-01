e3c69d3803b219ab5f2886667ab85e238230df91
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-01 14:27:30 +0530

    updated

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -22,6 +22,8 @@ import { ChangeEvent, useState } from 'react';
 import { Textarea } from '../ui/textarea';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
+import { updateUser } from '@/lib/actions/user.actions';
+import { usePathname, useRouter } from 'next/navigation';
 
 
 interface Props {
