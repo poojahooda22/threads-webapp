cb01982ea0791c6bcdee186af5f43a2645bc9c9c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-02 12:28:22 +0530

    create page

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,16 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from 'next/navigation';
+import { fetchUser } from '@/lib/actions/user.actions';
+
+async function Page() {
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if(!userInfo?.onboarded) redirect ('/onboarding');
+    return <h1 className="head-text">Create Thread</h1>
+}
+
+export default Page;
