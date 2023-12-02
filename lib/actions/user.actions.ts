ed51e90cf524f41fdde00458e6bee1dde9e522ed
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-02 12:29:09 +0530

    updated useractions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -46,3 +46,19 @@ export async function updateUser({
        throw new Error(`Failed to create/update user: ${error.message}`)
     }
 }
+
+
+export async function fetchUser(userId: string) {
+     try {
+        connectToDB();
+
+        return await User
+            .findOne({ id: userId })
+            .populate({
+                path: 'communities',
+                model: 'Community'
+            })
+     } catch (error: any) {
+            throw new Error(`Failed to fetch user: ${error.message}`)
+     }
+}
