6fc8a4395a8be0794e1fafdc0815f8cb1367e612
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-03 15:40:13 +0530

    updated

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -33,3 +33,34 @@ export async function createThread({text, author, communityId, path}: Params) {
         throw new Error(`Error creating thread: ${error.message}`)
     }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    //calculate the number of posts to skip
+    // const skipAmount = pageSize * (pageNumber - 1);
+
+    //fetch the posts that have no parentts (top-level threads...)
+
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined]}})
+        .sort({ createdAt: 'desc' })
+        // .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User})
+        // .populate({
+        //     path: 'children',
+        //     populate: {
+        //         path: 'author',
+        //         model: User,
+        //         select: "_id name parentId image"
+        //     }
+        // })
+
+        // const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined]} })
+
+        // const posts = await postsQuery.exec();
+
+        // const isNext = totalPostsCount > skipAmount + posts.length;
+
+        // return{ posts, isNext };
+}
