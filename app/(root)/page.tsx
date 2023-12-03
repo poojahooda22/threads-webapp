19224204a1f252ffe753d341f10977250753166f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-03 15:45:38 +0530

    updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,8 @@ import { fetchPosts } from "@/lib/actions/thread.actions"
 export default function Home() {
 
   //const result = await fetchPosts();
+
+  console.log(result);
   return (
     <>
        <h1 className='head-text text-left'>Home</h1>
