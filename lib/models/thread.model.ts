4fd6db418dfcb6b30218ec4deae256c5a029a9c8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-03 14:32:33 +0530

    updated thread model

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -1,7 +1,32 @@
 import mongoose from 'mongoose';
 
 const threadSchema = new mongoose.Schema({ 
-
+    text: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    communityId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Community',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    parentId: {
+        type: String
+    },
+    children: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thread'
+        } 
+    ]
 });
 
 const Thread = mongoose.models.Thread || mongoose.model('Thread', threadSchema);
