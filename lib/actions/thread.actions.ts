e20b2c824d46e2e78256ca54c1cdd45ee7080bff
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-03 14:32:20 +0530

    updated thread actions

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -2,10 +2,30 @@
 
 import Thread from "../models/thread.model";
 import { connectToDB } from "../mongoose";
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
 
 
 interface Params {
     text: string,
     author: string,
-    community: string,
+    communityId: string | null,
+    path: string,
+}
+
+export async function createThread({text, author, communityId, path}: Params) {
+    connectToDB();
+
+    const createThread = await Thread.create({
+        text,
+        author,
+        community: null,
+    });
+
+    //Update user model
+    await User.findByIdAndUpdate(author, {
+        $push: { threads: createThread._id }
+    })
+
+    revalidatePath(path);
 }
