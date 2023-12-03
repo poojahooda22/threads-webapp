8ac24e129c71609455f007b00060758667d25481
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-03 14:32:07 +0530

    updated post

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -24,6 +24,8 @@ import { Textarea } from '../ui/textarea';
 
 import { updateUser } from '@/lib/actions/user.actions';
 import { usePathname, useRouter } from 'next/navigation';
+import { createThread } from '@/lib/actions/thread.actions';
+import path from 'path';
 
 
 interface Props {
@@ -51,7 +53,53 @@ function PostThread({ userId }: { userId: string}) {
       }
     })
 
-    return <h1>Post Thread form</h1>
+    const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+      await createThread({
+        text: values.thread,
+        author: userId,
+        communityId: null,
+        path: pathname
+      });
+
+      router.push("/");
+    }
+
+    return (
+      <Form {...form}> 
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className=' mt-10 flex flex-col justify-start gap-10'
+        >
+          <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className='flex flex-col items-start gap-3 w-full'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Content
+              </FormLabel>
+              <FormControl 
+                className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                <Textarea
+                  rows={15} 
+                  className='account-form_input no-focus'
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button 
+          type="submit"
+          className='bg-primary-500'
+        >
+          Post Thread
+        </Button>
+        </form>
+      </Form>
+    )
 }
 
 export default PostThread;
