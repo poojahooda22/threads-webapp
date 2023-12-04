3dbf12a5af27c9f3fc120ee3f0668bdd967066d5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-04 14:35:52 +0530

    updated

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,5 +1,37 @@
-const Page = ({params} : {params: {id: string}}) => {
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+
+const Page = async ({params} : {params: {id: string}}) => {
+    if(!params.id) return null;
+
+    const user = await currentUser();
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    const thread = await fetchThreadById(params.id);
+
+    return (
+        <section className="relative">
+            <div>
+                <ThreadCard
+                    key={thread._id}
+                    id={thread._id}
+                    currentUserId={user?.id || ""}
+                    parentId={thread.parentId}
+                    content={thread.text}
+                    author={thread.author}
+                    community={thread.community}
+                    createdAt={thread.createdAt}
+                    comments={thread.children}
+                />
+            </div>
+        </section>
+    )
 }
 
 export default Page;
