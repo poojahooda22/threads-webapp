7b0cfbdabb40bfc308d32484ac86ca534c05dc40
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-05 14:16:33 +0530

    updated

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -44,7 +44,9 @@ const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
 
     const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
         await addCommentToThread(threadId, values.thread, JSON.parse(currentUserId), pathname);
-        router.push("/")
+
+
+        form.reset();
     };
 
     return (
