3b5e00e468aef2ff6285e2ceb1f9d0372fafdfc6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-05 13:13:10 +0530

    updated thread

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -5,9 +5,9 @@ export const ThreadValidation = z.object({
     thread: z.string().nonempty().min(3, {message:
     'Minimum 3 characters'}),
     accountId: z.string(),
-})
+});
 
 export const CommentValidation = z.object({
     thread: z.string().nonempty().min(3, {message:
     'Minimum 3 characters'}),
-})
+});
