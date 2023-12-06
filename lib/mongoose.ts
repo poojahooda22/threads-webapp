6b43f54406788cd1391797880f32523854b2e055
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-06 13:00:18 +0530

    updated mongoose

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -7,7 +7,7 @@ export const connectToDB = async () => {
 
     if(!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
 
-    if(isConnected) return console.log('Already connected to MongoDB');
+    if(isConnected) return;
 
     try {
         await mongoose.connect(process.env.MONGODB_URL);
