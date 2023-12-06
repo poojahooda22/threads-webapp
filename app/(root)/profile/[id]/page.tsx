d4bbab0122ff246bf45d6751743d60be0d23bfb9
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-06 19:46:30 +0530

    updated

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,6 +1,9 @@
 import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsTrigger, TabsList } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
 import { fetchUser } from "@/lib/actions/user.actions";  
 import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 
@@ -12,6 +15,7 @@ async function Page({params} : {params: {id: string}}) {
     const userInfo = await fetchUser(user.id);
 
     if(!userInfo?.onboarded) redirect('/onboarding');
+
     return (
         <section>
             <ProfileHeader
@@ -23,8 +27,24 @@ async function Page({params} : {params: {id: string}}) {
                 bio={userInfo.bio}
             />
 
-            <div>
-                
+            <div className="mt-9">
+                <Tabs defaultValue="threads" className="w-full">
+                    <TabsList className="tab">
+                        {profileTabs.map((tab) => (
+                            <TabsTrigger className="tab" key={tab.label} value={tab.value}>
+                                <Image
+                                    src={tab.icon}
+                                    alt={tab.label}
+                                    width={24}
+                                    height={24}
+                                    className="object-contain"
+                                />
+                            </TabsTrigger>
+                        ))}
+
+                    </TabsList>
+                </Tabs>
+
             </div>
         </section>
     )
