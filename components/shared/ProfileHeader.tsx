771e0391806cef2e22be43223a5eb8c0dde89736
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-07 12:21:36 +0530

    updated

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface Props {
     accountId: string;
     authUserId: string;
@@ -9,9 +11,36 @@ interface Props {
 
 const ProfileHeader = ({ accountId, authUserId, name, username, imgUrl, bio}: Props) => {
     return (
-        <div>
-            <h1>Profile Header</h1>
-        </div>
+        <div className="flex w-full flex-col justify-start">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                    <div>
+                        <Image
+                            src={imgUrl}
+                            alt="Profile Image"
+                            fill
+                            className="rounded-full object-cover shadow-2xl"
+                        />
+                    </div>
+
+                    <div className="flex-1">
+                        <h1 className="text-left text-heading3-bold text-light-1">{name}</h1>
+                        <p className="text-base-medium text-gray-1">@{username}</p>
+                    </div>
+                </div>
+
+                {/* TODO: Community */}
+                <p
+                    className="mt-6 max-ww-lg text-base-regular text-light-2"
+                >
+                    {bio}
+                </p>
+
+                <div className="mt-12 h-0.5 w-full bg-dark-3" />
+
+                
+            </div>
+        </div> 
     )
 }
 
