e491b41da0c39378fc9c283efd2dffa3a415d515
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-07 21:54:16 +0530

    updated

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -25,7 +25,7 @@ const ProfileHeader = ({ accountId, authUserId, name, username, imgUrl, bio}: Pr
 
                     <div 
                         className="flex-1"
-                        >
+                    >
                         <h1 className="text-left text-heading3-bold text-light-1">{name}</h1>
                         <p className="text-base-medium text-gray-1">@{username}</p>
                     </div>
