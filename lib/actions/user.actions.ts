84aa86f365532b6b138719fd563365464ee1c0da
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-07 12:22:00 +0530

    updated

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,7 +4,7 @@ import  User  from '../models/user.model'
 
 import { connectToDB } from '../mongoose';
 import { revalidatePath } from 'next/cache'
-
+import  Thread  from '../models/thread.model';
 
 interface Params {
     userId: string;
@@ -61,4 +61,29 @@ export async function fetchUser(userId: string) {
      } catch (error: any) {
             throw new Error(`Failed to fetch user: ${error.message}`)
      }
+}
+
+export async function fetchUserPosts(userId: string) {
+    try{
+        connectToDB();
+
+        //TODO Populate community
+
+        const threads = await User.findOne({id: userId})
+        .populate({
+            path: 'threads',
+            model: 'Thread',
+            populate: {
+                path: 'children',
+                model: Thread,
+                populate: {
+                    path: 'author',
+                    model: User,
+                    select: 'name image id'
+                }
+            }
+        })
+    } catch (error) {
+         
+    }
 }
