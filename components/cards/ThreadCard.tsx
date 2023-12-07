917de77196ab40b39c1d1171e6f0cf99b8931d5e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-07 13:29:44 +0530

    updated threadcard

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -74,7 +74,7 @@ const ThreadCard = ({
                             {content}
                         </p>
 
-                        <div className="mt-5 flex flex-col gap-3">
+                        <div className={`${isComment && 'mb-10'} mt-5 flex flex-col gap-3`}>
                             <div className="flex gap-3.5">
                                 <Image 
                                     src="/assets/heart-gray.svg"
