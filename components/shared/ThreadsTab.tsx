0afa96ec744b9471dae51c6ea7324c693760f072
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-07 12:21:43 +0530

    updated

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,4 +1,17 @@
-const ThreadsTab = async () => {
+
+import { fetchUserPosts } from "@/lib/actions/user.actions";
+
+interface Props {
+    currentUserId: string;
+    accountId: string;
+    accountType: string;
+}
+
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType}: Props) => {
+    let result = await fetchUserPosts(accountId);
+
+    
     return (
         <section>
             ThreadsTab
