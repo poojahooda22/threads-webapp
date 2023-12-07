2782fbc9b18ee1ec823c250f8db704cec15bd733
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-07 12:45:56 +0530

    updated

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,6 +1,7 @@
 
 import { fetchUserPosts } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
+import ThreadCard from "../cards/ThreadCard";
 
 interface Props {
     currentUserId: string;
@@ -15,7 +16,19 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType}: Props) => {
     if(!result) redirect('/')
     return (
         <section className="mt-9 flex flex-col gap-10">
-            ThreadsTab
+           {result.threads.map((thread) => (
+            <ThreadCard
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id || ""}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+            />
+           ))}
         </section>
     )
 }
