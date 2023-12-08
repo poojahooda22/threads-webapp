30a5e65b66919cc848e8f13ce8f82df0cdef1909
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-08 22:57:54 +0530

    updated

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,6 +1,18 @@
 // import User from "@/lib/models/user.model"
 
-function UserCard () {
+interface Props {
+    id: string;
+    name: string;
+    username: string;
+    imgUrl: string;
+    personType: string;
+
+}
+
+const UserCard = ({
+    id,
+    name, username, imgUrl, personType
+}: Props) => {
     return (
         <div>
             UserCard
