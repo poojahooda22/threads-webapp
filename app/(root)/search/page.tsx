78a00195f773218098a8995883dfa8344cec3ee8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-08 19:55:45 +0530

    updated

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,52 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from 'next/navigation';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import PostThread from "@/components/forms/PostThread";
+import Image from 'next/image';
+import { profileTabs } from '@/constants';
+import ThreadsTab from "@/components/shared/ThreadsTab";
 
 
-const Page = async () => {
+async function Page() {
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if(!userInfo?.onboarded) redirect ('/onboarding');
+
+    //fetch users
+    const result = await fetchUsers({
+      userId: user.id,
+      searchString: '',
+      pageNumber: 1,
+      pageSize: 25,
+    });
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search bar */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              // <UserCard
+              //   key={person.id}
+              //   id={person.id}
+              //   name={person.name}
+              //   username={person.username}
+              //   imgUrl={person.image}
+              //   personType='User'
+              // />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   )
 }
