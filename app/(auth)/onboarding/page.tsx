614ddc1dd0b14f93ddb767ff51a98d657f838a07
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-08 13:43:38 +0530

    updated

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,15 @@
 import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page() {
     const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
 
-    const userInfo = {};
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
     const userData = {
        id: user?.id,
