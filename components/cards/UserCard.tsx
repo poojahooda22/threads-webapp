125ad24717ffd3f0fb98885d86e2bd209d41cf39
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-08 22:27:30 +0530

    updated

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,3 +1,5 @@
+import User from "@/lib/models/user.model"
+
 function UserCard () {
     return (
         <div>
@@ -6,4 +8,4 @@ function UserCard () {
     )
 }
 
-export default 
+export default UserCard
