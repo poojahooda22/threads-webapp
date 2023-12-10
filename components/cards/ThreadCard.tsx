577799ff3779fd76297302d03f87cd93d2dd547f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-10 22:47:20 +0530

    updated

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -110,7 +110,8 @@ const ThreadCard = ({
 
                             {isComment &&  comments.length > 0 && (
                                 <Link href={`/thread/${id}`}>
-                                    <p className="mt-1 text-subtle-medium text-gray-1">
+                                    <p 
+                                    className="mt-1 text-subtle-medium text-gray-1">
                                         {comments.length} replies
                                     </p>
                                 </Link>
