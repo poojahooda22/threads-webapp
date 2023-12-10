6e5144aa4b39b061684ae9b5611f454a02106d26
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-10 22:49:20 +0530

    updated

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -40,7 +40,8 @@ const ThreadCard = ({
     return (
         <article 
             className={`flex w-full flex-col rounded-xl  ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
-            <div className="flex items-center justify-between">
+            <div 
+            className="flex items-center justify-between">
                 <div className="flex w-full flex-1 flex-row gap-4">
                     <div className="flex flex-col items-center">
                         <Link
