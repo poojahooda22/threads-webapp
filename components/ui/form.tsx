89f5922ea03799e242b0b1c9eaeff1df71aad86b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-14 21:55:03 +0530

    updated form

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -8,7 +8,7 @@ import {
   FieldValues,
   FormProvider,
   useFormContext,
-} from "react-hook-form"
+} from "react-hook-form";
 
 import { cn } from "@/lib/utils"
 import { Label } from "@/components/ui/label"
