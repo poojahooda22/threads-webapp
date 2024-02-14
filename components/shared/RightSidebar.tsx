a6e1f25a06a2bae938ad71871aa3d4337167cc99
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-14 22:13:02 +0530

    updated

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -17,7 +17,8 @@ function RightSidebar() {
 
             <div className="flex flex-1 flex-col justify-start">
                 <h3 
-                    className="text-heading4-medium text-light-1"
+                    className="text-heading4-medium 
+                    text-light-1"
                 >
                     Suggested Users
                 </h3>
