6d74bd222ea0244386538856997b03cb480044df
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-14 22:13:45 +0530

    updated

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -18,7 +18,7 @@ function RightSidebar() {
             <div 
                 className="flex flex-1 flex-col 
                 justify-start"
-                >
+            >
                 <h3 
                     className="text-heading4-medium 
                     text-light-1"
