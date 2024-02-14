ffa378f315fe696cb732160a77fd26887c7a320d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-14 22:13:12 +0530

    updated

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -15,7 +15,8 @@ function RightSidebar() {
                     >Suggested Communities</h3>
             </div>
 
-            <div className="flex flex-1 flex-col justify-start">
+            <div 
+            className="flex flex-1 flex-col justify-start">
                 <h3 
                     className="text-heading4-medium 
                     text-light-1"
