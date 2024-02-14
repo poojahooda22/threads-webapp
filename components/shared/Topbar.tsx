84a767d8dfebd8d728fac641d5228a2d85731877
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-14 22:10:19 +0530

    updated

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -14,7 +14,8 @@ function Topbar() {
                     width={28}
                     height={28}
                 />
-                <p className="text-headings3-bold text-light-1 
+                <p 
+                className="text-headings3-bold text-light-1 
                 max-xs:hidden">
                     Threads
                 </p>
